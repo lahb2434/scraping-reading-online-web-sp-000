a2675ab5b4fa68b2b4bf9b313bff58035789f58f
Chris Houghton <chrishoughton2434@gmail.com>
2020-10-24 22:36:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 courses = doc.css(".gridContainer-EnvccM .title-oE5vT4")
 
 courses.each do |course| 
-  puts 'The best #{course.text.strip.upcase} you ever bet you\'d see again!!'
+  puts "The best #{course.text.strip.upcase} you ever bet you\'d see again!!"
 end
 
 
