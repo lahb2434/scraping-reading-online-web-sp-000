fd34745ef2458d4ee52640d07086ab1c4beb1361
Chris Houghton <chrishoughton2434@gmail.com>
2020-10-24 22:11:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 courses = doc.css("id-3c239880-48cf-5ce7-84d0-578546d87586.inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
 courses.each do |course| 
-  puts 'yes' course.text.strip
+  puts course.text.strip
 end
 
 
